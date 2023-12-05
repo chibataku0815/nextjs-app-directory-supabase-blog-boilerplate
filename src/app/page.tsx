2eae29e320bfbe9453d11dbd69bd63c47049f56a
chibataku0815 <chiba@fores-tone.co.jp>
2023-12-05 16:59:53 +0900

    Add theme configuration and NextThemeProvider component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,43 @@
-import { Button, Flex, Text } from '@radix-ui/themes';
-import { createServerClient } from '@supabase/ssr';
-import { cookies } from 'next/headers';
+import { Button, Flex, Text, Theme, ThemePanel } from '@radix-ui/themes';
+import styles from './page.module.css';
+import './theme-config.css';
 
 export default async function Page() {
-  const cookieStore = cookies();
+  // const cookieStore = cookies();
 
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        get(name: string) {
-          return cookieStore.get(name)?.value;
-        },
-      },
-    }
-  );
+  // const supabase = createServerClient(
+  //   process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  //   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  //   {
+  //     cookies: {
+  //       get(name: string) {
+  //         return cookieStore.get(name)?.value;
+  //       },
+  //     },
+  //   }
+  // );
 
   return (
-    <Flex direction="column" gap="2">
-      <Text>Hello from Radix Themes :)</Text>
-      <Button>Let's go</Button>
-    </Flex>
+    <html lang="ja" suppressHydrationWarning>
+      <body className={styles.body}>
+        <Theme
+          asChild
+          appearance="dark"
+          accentColor="mint"
+          radius="large"
+          scaling="110%"
+        >
+          <div id="root">
+            <ThemePanel />
+            <>
+              <Flex direction="column" gap="2">
+                <Text>Hello from Radix Themes :</Text>
+                <Button>Lets go</Button>
+              </Flex>
+            </>
+          </div>
+        </Theme>
+      </body>
+    </html>
   );
 }
